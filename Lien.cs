a0188890c1a9fab2f49088fa6add726fa7cc4b03
Camcode21 <camille.negrie@edu.devinci.fr>
2025-04-01 21:22:15 +0200

    Update Lien.cs

--- Lien.cs
+++ Lien.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 /// Représente un lien entre deux nœuds dans un graphe.
 namespace LivinParisVfinale
 {
-    public class Lien
+    public class Lien<T>
     {
         /// Identifiant du premier nœud    
-        public int Noeud1 { get; set; }
+        public T Noeud1 { get; set; }
         /// Identifiant du deuxième nœud.
-        public int Noeud2 { get; set; }
-        public Lien(int noeud1, int noeud2)
+        public T Noeud2 { get; set; }
+        
+        public Lien(T noeud1, T noeud2)
         {
             Noeud1 = noeud1;
             Noeud2 = noeud2;
