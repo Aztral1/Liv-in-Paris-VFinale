abdf9f8b4d110682559a4c8e47df30ae085a4168
Aztral1 <ywmoysan@gmail.com>
2025-04-03 23:27:41 +0200

    Update Noeud.cs

--- Noeud.cs
+++ Noeud.cs
@@ -1,21 +1,35 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace LivinParisVfinale
-{   /// Représente un nœud dans un graphe.
-    public class Noeud<T>
+public class Noeud<T>
+{
+    public int Id { get; }
+    public T Libelle { get; }
+    public string LibelleLigne { get; }
+    public double Longitude { get; }
+    public double Latitude { get; }
+    public string Commune { get; }
+    public string CodeInsee { get; }
+    public List<Lien<T>> Liens { get; } = new List<Lien<T>>();
+    public double TempsChangement { get; set; }
+    public Noeud(int id, T libelle, string libelleLigne, double longitude, double latitude, string commune, string codeInsee, double tempschangement)
     {
-        /// Identifiant unique du nœud.
-        public T IdNoeud { get; set; }
-        /// Liste des nœuds adjacents.
-        public List<Noeud<T>> Adjacents { get; set; } = new List<Noeud<T>>();
-        /// Constructeur pour créer un nœud avec un identifiant donné.
-        public Noeud(T idnoeud)
+        Random r = new Random();
+        Id = id;
+        Libelle = libelle;
+        LibelleLigne = libelleLigne;
+        Longitude = longitude;
+        Latitude = latitude;
+        Commune = commune;
+        CodeInsee = codeInsee;
+        TempsChangement = tempschangement;
+    }
+
+    public void AjouterLien(Noeud<T> destination, double poids, bool bidirectionnel = false)
+    {
+        Liens.Add(new Lien<T>(this, destination, poids));
+        if (bidirectionnel)
         {
-            IdNoeud = idnoeud;
+            destination.Liens.Add(new Lien<T>(destination, this, poids));
         }
     }
 }
