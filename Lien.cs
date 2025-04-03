1f89f370ebb2666bbedf411cb4b10b04db78876a
Aztral1 <ywmoysan@gmail.com>
2025-04-03 23:29:31 +0200

    Update Lien.cs

--- Lien.cs
+++ Lien.cs
@@ -1,22 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-/// Représente un lien entre deux nœuds dans un graphe.
-namespace LivinParisVfinale
-{
+
     public class Lien<T>
-    {
+    {    
         /// Identifiant du premier nœud    
         public T Noeud1 { get; set; }
         /// Identifiant du deuxième nœud.
         public T Noeud2 { get; set; }
         
-        public Lien(T noeud1, T noeud2)
+        public double Poids { get; }
+        public Lien(T noeud1, T noeud2, double poids)
         {
             Noeud1 = noeud1;
             Noeud2 = noeud2;
+            Poids = poids;
         }
     }
-}
+
