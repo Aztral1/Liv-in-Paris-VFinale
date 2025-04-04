2284f02d343d531125f8fc0606c6659c1e5db0a7
Aztral1 <ywmoysan@gmail.com>
2025-04-04 17:21:43 +0200

    Update Chemin.cs

--- Chemin.cs
+++ Chemin.cs
@@ -1,8 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public static class Dijkstra<T>
+public static class Chemin<T>
 {
     public static List<Noeud<T>> Dijsktra(Graphe<T> graphe, Noeud<T> depart, Noeud<T> arrivee)
     {
@@ -73,48 +73,145 @@ public static class Dijkstra<T>
     public static Dictionary<Noeud<T>, double> BellmanFord(Graphe<T> graphe, Noeud<T> source)
     {
         var distances = new Dictionary<Noeud<T>, double>();
-        foreach (var noeud in graphe.Noeuds)
-            distances[noeud] = (noeud == source) ? 0 : double.MaxValue;
+        foreach (var node in graphe.Noeuds)
+            distances[node] = (node == source) ? 0 : double.MaxValue;
 
         for (int i = 0; i < graphe.Noeuds.Count - 1; i++)
         {
-            bool modifie = false;
+            bool updated = false;
             foreach (var lien in graphe.Liens)
             {
                 if (distances[lien.Source] != double.MaxValue &&
                     distances[lien.Source] + lien.Poids < distances[lien.Destination])
                 {
                     distances[lien.Destination] = distances[lien.Source] + lien.Poids;
-                    modifie = true;
+                    updated = true;
                 }
             }
-            if (!modifie) break; // Optimisation
+            if (!updated) break; // Optimisation
         }
 
         return distances;
     }
-    public static Dictionary<Noeud<T>, Dictionary<Noeud<T>, double>> FloydMarshall(Graphe<T> graphe)
+    public static List<Noeud<string>> ReconstruireCheminBellmanFord(
+    Dictionary<Noeud<string>, double> distances,
+    Graphe<string> graphe,
+    Noeud<string> depart,
+    Noeud<string> arrivee)
     {
-        var dist = new Dictionary<Noeud<T>, Dictionary<Noeud<T>, double>>();
+        double INF = double.MaxValue / 2;
+
+        if (distances[arrivee] >= INF)
+        {
+            return new List<Noeud<string>>();
+        }
+
+        var chemin = new List<Noeud<string>> { arrivee };
+        var courant = arrivee;
+        int limite = graphe.Noeuds.Count;
+
+        while (courant != depart && limite-- > 0)
+        {
+            var precedent = graphe.Noeuds
+                .Where(n => n.Liens.Any(l => l.Destination == courant))
+                .OrderBy(n => distances[n] + n.Liens.First(l => l.Destination == courant).Poids)
+                .FirstOrDefault();
+
+            if (precedent == null || distances[precedent] >= INF)
+            {
+                return new List<Noeud<string>>();
+            }
+
+            chemin.Insert(0, precedent);
+            courant = precedent;
+        }
+
+        if (limite <= 0 || courant != depart)
+        {
+            return new List<Noeud<string>>();
+        }
+
+        return chemin;
+    }
+    public static (Dictionary<(Noeud<T>, Noeud<T>), double> distances,
+                  Dictionary<(Noeud<T>, Noeud<T>), Noeud<T>> predecesseurs)
+        FloydWarshall(Graphe<T> graphe)
+    {
+        var distances = new Dictionary<(Noeud<T>, Noeud<T>), double>();
+        var predecesseurs = new Dictionary<(Noeud<T>, Noeud<T>), Noeud<T>>();
 
         // Initialisation
         foreach (var u in graphe.Noeuds)
         {
-            dist[u] = new Dictionary<Noeud<T>, double>();
             foreach (var v in graphe.Noeuds)
-                dist[u][v] = (u == v) ? 0 : double.MaxValue;
+            {
+                if (u == v)
+                {
+                    distances[(u, v)] = 0;
+                }
+                else
+                {
+                    // Vérifie s'il y a un lien direct
+                    var lienDirect = u.Liens.FirstOrDefault(l => l.Destination == v);
+                    if (lienDirect != null)
+                    {
+                        distances[(u, v)] = lienDirect.Poids;
+                        predecesseurs[(u, v)] = u;
+                    }
+                    else
+                    {
+                        distances[(u, v)] = double.MaxValue;
+                    }
+                }
+            }
         }
 
-        foreach (var lien in graphe.Liens)
-            dist[lien.Source][lien.Destination] = lien.Poids;
-
-        // Algorithme
+        // Algorithme principal
         foreach (var k in graphe.Noeuds)
+        {
             foreach (var i in graphe.Noeuds)
+            {
                 foreach (var j in graphe.Noeuds)
-                    if (dist[i][k] != double.MaxValue && dist[k][j] != double.MaxValue)
-                        dist[i][j] = Math.Min(dist[i][j], dist[i][k] + dist[k][j]);
+                {
+                    // Prend en compte le temps de changement si on change de ligne
+                    double changement = (i != k && k != j && i.Lignes.Intersect(k.Lignes).Count() == 0)
+                        ? k.TempsChangement
+                        : 0;
 
-        return dist;
+                    if (distances[(i, k)] + distances[(k, j)] + changement < distances[(i, j)])
+                    {
+                        distances[(i, j)] = distances[(i, k)] + distances[(k, j)] + changement;
+                        predecesseurs[(i, j)] = predecesseurs[(k, j)];
+                    }
+                }
+            }
+        }
+
+        return (distances, predecesseurs);
     }
-}
+
+    public static List<Noeud<T>> ReconstruireCheminFloydWarshall(
+        Dictionary<(Noeud<T>, Noeud<T>), Noeud<T>> predecesseurs,
+        Noeud<T> depart, Noeud<T> arrivee)
+    {
+        var chemin = new List<Noeud<T>>();
+
+        if (predecesseurs.TryGetValue((depart, arrivee), out var precedent))
+        {
+            if (precedent == null)
+            {
+                return new List<Noeud<T>>(); // Pas de chemin
+            }
+
+            var courant = arrivee;
+            while (courant != depart)
+            {
+                chemin.Insert(0, courant);
+                courant = predecesseurs[(depart, courant)];
+            }
+            chemin.Insert(0, depart);
+        }
+
+        return chemin;
+    }
+}   
