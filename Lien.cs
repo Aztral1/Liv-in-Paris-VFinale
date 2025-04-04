f3db21d49df0d99770c282e20e30869e318a735f
Camcode21 <camille.negrie@edu.devinci.fr>
2025-04-04 11:29:40 +0200

    Update Lien.cs

--- Lien.cs
+++ Lien.cs
@@ -5,8 +5,11 @@
         public T Noeud1 { get; set; }
         /// Identifiant du deuxième nœud.
         public T Noeud2 { get; set; }
-        
+
+        /// Poids du lien entre les deux nœuds
         public double Poids { get; }
+
+        /// Constructeur qui initialise les deux nœuds et le poids du lien
         public Lien(T noeud1, T noeud2, double poids)
         {
             Noeud1 = noeud1;
